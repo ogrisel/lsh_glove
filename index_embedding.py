e25e6c5f48ff667060bbb5e7712b775513706219
Olivier Grisel <olivier.grisel@ensta.org>
2014-11-14 19:49:56 +0100

    download the data

--- index_embedding.py
+++ index_embedding.py
@@ -1,11 +1,15 @@
 from gzip import GzipFile
 import numpy as np
 import os
+import os.path as op
 from sklearn.neighbors import LSHForest, NearestNeighbors
 from sklearn.cross_validation import train_test_split
 from time import time
+from urllib.request import urlretrieve
 
 from joblib import Memory
+
+GLOVE_6B_300D_URL = "http://www-nlp.stanford.edu/data/glove.6B.300d.txt.gz"
 
 m = Memory(cachedir='.')
 
@@ -28,12 +32,14 @@ def load_glove_embedding(filepath):
 
 @m.cache
 def build_index(data, n_estimators=20, n_candidates=100, n_neighbors=10, seed=0):
+    print("Building LSHF(n_estimators=%dn_candidates) on data with shape=%r."
+          % (n_estimators, data.shape))
     lshf = LSHForest(n_estimators=n_estimators, n_candidates=n_candidates,
                      n_neighbors=n_neighbors, random_state=seed)
     t0 = time()
     lshf.fit(data)
     duration = time() - t0
-    print("Build LSHF(n_estimators=%d) on data with shape=%r in %0.3fs"
+    print("Built LSHF(n_estimators=%d) on data with shape=%r in %0.3fs"
           % (n_estimators, data.shape, duration))
     return lshf, duration
 
@@ -52,7 +58,17 @@ def query_exact(data, query, n_neighbors=10):
 if __name__ == '__main__':
     import sys
     n_queries = 100
-    filepath = os.path.abspath(sys.argv[1])
+    if len(sys.argv) > 1:
+        filepath = os.path.abspath(sys.argv[1])
+    else:
+        data_folder = op.expanduser('~/data')
+        if not op.exists(data_folder):
+            os.makedirs(data_folder)
+        filename = op.basename(GLOVE_6B_300D_URL)
+        filepath = op.join(data_folder, filename)
+        if not op.exists(filepath):
+            print('Downloading %s' % GLOVE_6B_300D_URL)
+            urlretrieve(GLOVE_6B_300D_URL, filepath)
     words, vectors = load_glove_embedding(filepath)
     words = np.array(words, dtype='object')
 
